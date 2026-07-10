af24fe14f8d1e91abc638ec24a5df6b76c1a1074
Aaron Upshaw <aaronup87@yahoo.com>
2026-07-10 15:18:37 -0500

    Fix GitHub Pages deployment and base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // For GitHub Pages, change base to your repo name, for example:
 // base: "/personal-tarp-app/"
 export default defineConfig({
-  base: "/",
+  base: "/TARP/",
   plugins: [react()]
 });
